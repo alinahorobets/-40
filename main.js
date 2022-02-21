e46fcfc707a05453984f6381af50e7af933b81c3
alina horobets <92310415+DragoKane@users.noreply.github.com>
2022-02-21 19:55:23 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+//const btn = document.querySelector('.btn')
+//const click = document.querySelector('.click')
+
+
+//btn.addEventListener ('click', () => {
+  //  let time = 0
+    //setInterval(() => {
+      //  clock.textContent = time
+        //time++
+    //}, 1000)
+    //btn.removeEventListener('click', timerClock)
+//})
+
+//btn.addEventListener('click', timerClock)
+
+//const btn = document.querySelector('.btn')
+//const poppUp = document.querySelector('.pop_Up')
+
+//btn.addEventListener('click', () => {
+//    setTimeout(() =>{
+//        poppUp.classList.add('show__pop_up')
+//        setTimeout(() =>{
+//            poppUp.classList.remove('show__pop_up')
+//        },2000)
+//    },2000)
+//})
+
+// const btn = document.querySelector('.btn')
+// const poppUp = document.querySelector('.clock')
+
+// const showDate = () =>{
+//     let today = new Date()
+//     let day = today.getDate()
+    
+//     clock.textContent = day
+// }
+
+// btn.addEventListener('click',showDate)
